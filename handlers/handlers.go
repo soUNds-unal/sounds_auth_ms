32ed3a6a407c574ae0688ba2a0350578afe04843
ccmorenov <ccmorenov@unal.edu.co>
2022-06-07 09:55:27 -0500

    Fix token handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,9 +18,9 @@ func Manejadores() {
 	router.HandleFunc("/registro", middlew.ViewBD(routes.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.ViewBD(routes.Login)).Methods("POST")
 	router.HandleFunc("/verperfil", middlew.ViewBD(routes.VerPerfil)).Methods("GET")
-	router.HandleFunc("/modifyperfil", middlew.ViewBD(middlew.ValidateJWT(routes.ModifyPerfil))).Methods("PUT")
-	router.HandleFunc("/eliminaruser", middlew.ViewBD(middlew.ValidateJWT(routes.EliminarUser))).Methods("DELETE")
-	router.HandleFunc("/uploadavatar", middlew.ViewBD(middlew.ValidateJWT(routes.UploadAvatar))).Methods("POST")
+	router.HandleFunc("/modifyperfil", middlew.ViewBD(routes.ModifyPerfil)).Methods("PUT")
+	router.HandleFunc("/eliminaruser", middlew.ViewBD(routes.EliminarUser)).Methods("DELETE")
+	router.HandleFunc("/uploadavatar", middlew.ViewBD(routes.UploadAvatar)).Methods("POST")
 	router.HandleFunc("/getavatar", middlew.ViewBD(routes.GetAvatar)).Methods("GET")
 
 	PORT := os.Getenv("PORT")
--- middlew/validateJWT.go
+++ middlew/validateJWT.go
@@ -10,7 +10,7 @@ func ValidateJWT(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		_, _, _, err := routes.ProcesoToken(r.Header.Get("Authorization"))
 		if err != nil {
-			http.Error(w, "Error en el Token !"+err.Error(), http.StatusBadRequest)
+			http.Error(w, "Error en el Token!: "+err.Error(), http.StatusBadRequest)
 			return
 		}
 		next.ServeHTTP(w, r)
